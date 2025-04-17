5f44071504b4a9bc462e16d6596c4b9851e878a0
Kevin Ernest Long <kevinelong@gmail.com>
2025-04-17 16:25:22 -0700

    update1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+What is the Java Virtual Machine (JVM)?
+What are some primitive data types (all lower case)?
+What are some complex non-primitive Object based types (all title case)?
+Which keywords can be used to declare a class in Java?
+What rules are there for identifiers (variable names)?
+What are some tricky application of PMDAS in java expressions?
+What is the difference between ++i and i++? When would that matter?
+How would a java programmer shorten the statement x = x + 5; using the addition-assignment operator?
+What does .equalsIgnoreCase() do?
+What do we call the special methods used to create a new object instance?
+What is one way to declare an array?
+Which keyword refers to the current instance of an object?
+What other comparison operators exist other than == ?
+What is the purpose of a switch statement in Java?
+In an if, else if, else statement how many times can each of those occur respectively?
+In a loop, what is the difference between break and continue?
+What does the classic for look like? What are the three parts? How are they different?
+What does the modern for loop look like and what doesn't it have that the classic for has?
+What are && and || called and how are they different?
+What is the .equals() method used for?
+What are the methods for changing the "case" of a string?
+What is proper syntax for a main() method in Java. Hint the intellij macro is "psvm".
+How do you declare a simple class?
+How would you create an instance of that simple class?
+What are some examples of invalid variable/method names?
+How many methods of for displaying output do you know?
+Which kind of loop is guaranteed to execute at least once? (hint: they are the least used)
+Which loops can be used to iterate over the elements of an array in Java?
+What does the + operator do if a string is involved? What is it called?
+How and why is .substring() used?
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
